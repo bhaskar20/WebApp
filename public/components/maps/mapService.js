90d47399364a8d94a483a4f8e64a56761b26b321
bhaskar20 <bhaskar@logiport.in>
2016-02-18 03:28:34 -0500

    fix

--- public/components/maps/mapService.js
+++ public/components/maps/mapService.js
@@ -2,19 +2,19 @@
 angular.module('logiWebMain').run(["mapService", function (mapService) {
     //todo init
 }])
-angular.module('logiWebMain').factory('mapService', ["$q", "ongoingTripsService", "$http", function ($q, ongoingTripsService, $http) {
+angular.module('logiWebMain').factory('mapService', ["$q", "ongoingTripsService", "$http", "$interval", function ($q, ongoingTripsService, $http, $interval) {
     var ser = {};
 
-    ser.tripModels = [];
+    ser.gpsModels = [];
     ser.state = {
         stale: false,
         refreshing: false,
         error: null
     };
     var defer = $q.defer();
-
     var init = function () {
         ser.state.refreshing = true;
+        var canceller = $q.defer();
         ongoingTripsService.getongoingTrips().then(function(){
             var ongoingTrips = ongoingTripsService.ongoingTrips;
             var _gpsId = ongoingTrips.map(function (el) {
@@ -29,24 +29,58 @@ angular.module('logiWebMain').factory('mapService', ["$q", "ongoingTripsService"
                     time: Date.now()
                 },
                 responseType: 'json',
+                timeout:canceller.promise,
                 cache: false
             }).then(function (res) {
+                ser.gpsModels = ongoingTrips.map(function (el) {
+                    var copy = el;
+                    res.data.forEach(function (r) {
+                        if (r.gpsId = copy.gpsId) {
+                            _.extend(copy, r);
+                        }
+                    })
+                    //merge the objkects
+                });
                 ser.state.refreshing = false;
-                defer.resolve(res);
+                defer.resolve();
             }, function (err) {
                 ser.state.refreshing = false;
                 //handle error
-                defer.reject(err);
+                defer.reject();
             });
         })
     }
-    init();
+
+    $interval(function () {
+        canceller.resolve();
+        init();
+    },10000);
 
     ser.getInitLocations = function () {
         return defer.promise;
     }
-    ser.getSpecificLocations = function () {
-        //todo
+
+    ser.getSpecificLocations = function (gpdId,time) {
+        var defer = $q.defer();
+        $http({
+            method: "GET",
+            url: '/api/getDataAtTimeForOneGps/',
+            params: {
+                "gpsIds":gpdId,
+                //gpsIds: _gpsId;
+                //todo time
+                "time": Date.now()
+            },
+            responseType: 'json',
+            timeout: canceller.promise,
+            cache: false
+        }).then(function (res) {    
+            defer.resolve(res);
+        }, function (err) {
+            //handle error
+            defer.reject(err);
+        });
+        return defer.promise;
     }
     return ser;
 }])

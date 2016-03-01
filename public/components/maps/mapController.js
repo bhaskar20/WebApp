9360f6ffd9fc6fab618219f1539e48596b4537e1
neil4droid <cterse@gmail.com>
2016-03-01 20:45:18 +0530

    Map Changes

--- public/components/maps/mapController.js
+++ public/components/maps/mapController.js
@@ -3,7 +3,9 @@ angular
     .module("logiWebMain")
     .controller("mapController", ["$scope", "mapService", "uiGmapGoogleMapApi", function ($scope, mapService, uiGmapGoogleMapApi) {
         uiGmapGoogleMapApi.then(function (maps) {
-            $scope.mapOpts = { center: { latitude: 21, longitude: 78 }, zoom: 6 };
+            //$scope.marker = [];
+            $scope.marker = mapService.gpsModels;
+            $scope.mapOpts = { center: { latitude: 21, longitude: 78 }, zoom: 13 };
         });
         //set markers for each gpsid location https://angular-ui.github.io/angular-google-maps/#!/api/markers
 
